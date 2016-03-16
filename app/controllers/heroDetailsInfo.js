0e78038050f4414326451350c67579a70715355d
Leonardo Leal <dev@leoleal.com.br>
2016-03-15 23:42:09 -0300

    feat(details): add stats info

--- app/controllers/heroDetailsInfo.js
+++ app/controllers/heroDetailsInfo.js
@@ -7,9 +7,19 @@ Ti.API.info('[HERO DETAILS INFO CONTROLLER] Args: ' + JSON.stringify(selectedHer
 init();
 
 function init(){
+  var stats = selectedHero.stats;
+  
   $.loreData.text = selectedHero.lore[Ti.Locale.currentLanguage];
-  $.initialHealth = selectedHero.lore[Ti.Locale.currentLanguage];
-  $.healthIncrement = selectedHero.lore[Ti.Locale.currentLanguage];
-  $.initialMana = selectedHero.lore[Ti.Locale.currentLanguage];
-  $.manaIncrement = selectedHero.lore[Ti.Locale.currentLanguage];
+  Ti.API.info('[HERO DETAILS INFO CONTROLLER][STATS] ' + JSON.stringify(stats));
+  
+    $.initialHealth.text = stats.hp.pool[0];
+    $.initalHealthIncrement.text = stats.hp.pool[1];
+    $.healthRegen.text = stats.hp.regen[0];
+    $.healthRegenIncrement.text = stats.hp.regen[1];
+    $.initialMana.text = stats.mana.pool;
+    $.manaRegen.text = stats.mana.regen;
+    $.initialDamage.text = stats.attack.damage[0];
+    $.damageIncrement.text = stats.attack.damage[1];
+    $.damageSpeed.text = stats.attack.speed;
+  
 }
